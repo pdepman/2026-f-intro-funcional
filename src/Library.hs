b105c323c7883d641a1c2914c08b1ac358f336b2
Ariel Ivan Arellano <aarellano@frba.utn.edu.ar>
2026-04-15 13:50:07 -0300

    codigo clase con anotaciones

--- src/Library.hs
+++ src/Library.hs
@@ -11,10 +11,7 @@ cinco = 5
 doble :: Number -> Number
 doble numero = numero + numero
 
-
 pares = even
-
-x = x + 1
 
 -- 1)
 
@@ -30,13 +27,20 @@ minutosEn :: Number -> Number
 minutosEn horas = horas * 60
 
 -- 4
--- 4) “Se desea conocer cuál es el costo de ir de una distancia con una combi”. 
-
--- Por ejemplo, el costo de recorrer 200 km es de $41.000.
 
 costoDeRecorrido :: Number -> Number
 costoDeRecorrido = costoViaje.minutosEn.duracionRecorrido
 -- costoDeRecorrido distancia = 205 * distancia
 
--- Flexibilidad
--- Robustez
+-- Comparamos estas dos soluciones en terminos de flexibilidad y robustez
+
+-- El costo del recorrido esta definido por otras funciones. Si algún dia 
+-- se decide modificar la funcion costoViaje, en la primera solucion, impacta automáticamente en costoDeRecorrido.
+-- En la segunda versión tenemos tambien un "numero magico" (el 205) que si bien hoy sabemos de donde sale el dia
+-- en el que otra persona o yo mismo tenga que revisarlo no voy a saber a qué hacía referencia. Además
+-- como ese numero depende de calculos que ya estoy haciendo previamente, estoy repitiendo logica.
+-- La primera solucion es más flexible que la segunda porque hace que mi programa sea más sencillo de 
+-- modificarlo ante un cambio de requerimientos.
+-- También es más robusta porque es más fácil modificarlo sin introducir errores.
+
+
